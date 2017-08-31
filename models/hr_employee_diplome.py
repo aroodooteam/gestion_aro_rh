58a9bf82b3c3fc4f0e4ab22ebbdfa6357b51940c
aroodoo_asus_hary <aroodoo@asus.aro>
2017-08-31 10:43:47 +0300

    update field niveau

--- models/hr_employee_diplome.py
+++ models/hr_employee_diplome.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
-from openerp import api, exceptions, fields, models, _
+
+from openerp import fields, models
 
 
 class hrEmployeeDiplome(models.Model):
-    _name = 'hr.employee.diplome'
 
+    _name = 'hr.employee.diplome'
 
     employee_id = fields.Many2one('hr.employee', string=u'Employé')
     ordre = fields.Char(string='Ordre')
     annee = fields.Integer(string=u'Année')
-    diplome_id = fields.Many2one('qual.type', string=u'Diplôme')
+    diplome_id = fields.Many2one(comodel_name='qual.type', string=u'Diplôme')
+    niveau_id = fields.Many2one(comodel_name='hr.niveau.etudes', string='Niveau', related='diplome_id.niveau_id')
     branche_id = fields.Many2one('hr.employee.branche', string='Branche')
-    niveau = fields.Char(related='diplome_id.niveau_id.name', readonly=True, string='Niveau')
     specialite = fields.Char(string=u'Spécialité')
     lieu = fields.Char(string='Lieu')
