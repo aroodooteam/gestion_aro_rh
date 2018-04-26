2509d5676e82d375f209db76075694e4e6c6ef94
solofo <srakotoson@aro.mg>
2018-04-26 09:42:42 +0300

    Modification du champ animateur

--- models/hr_session_elearning.py
+++ models/hr_session_elearning.py
@@ -15,7 +15,7 @@ class hrSessionElearning(models.Model):
     branche_id = fields.Char(related='plan_formation_id.branche', string='Branche')
     date_debut_session = fields.Date(string=u'Date Début')
     date_fin_session = fields.Date(string='Date Fin')
-    instructor_id = fields.Many2one('res.partner', string='Animateur')
+    instructor_id = fields.Many2one('hr.formation.instructor', string='Animateur')
     attendee_ids = fields.Many2many('hr.employee', string='Attendee')
     cout_session = fields.Float(string=u'Coût', digits=(6,2))
     active = fields.Boolean(string='Actif', default=True)
